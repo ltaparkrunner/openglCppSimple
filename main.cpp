4da95898a2dd20f1ec8bc2176af2c991832d4733
ltaparkrunner <ltaparkrunner@gmail.com>
2026-06-07 23:18:27 +0300

    A simple textures.

--- main.cpp
+++ main.cpp
@@ -10,10 +10,10 @@
 
 // Define the vertices of a triangle
 GLfloat vertices[] = {
-	-0.5f,		-0.5f,		0.0f,		1.0f,	0.0f,	0.0f,	// left  
-	-0.5f ,		0.5f,		0.0f,		0.0f,	1.0f,	0.0f,
-	0.5f,		0.5f,		0.0f,		0.0f,	0.0f,	1.0f,	// right 
-	0.5f,		-0.5f,		0.0f,		1.0f,	1.0f,	1.0f, // top   
+	-0.5f,		-0.5f,		0.0f,		1.0f,	0.0f,	0.0f,		0.0f, 0.0f,	// left  
+	-0.5f ,		0.5f,		0.0f,		0.0f,	1.0f,	0.0f,		0.0f, 1.0f,
+	0.5f,		0.5f,		0.0f,		0.0f,	0.0f,	1.0f,		1.0f, 1.0f,	// right 
+	0.5f,		-0.5f,		0.0f,		1.0f,	1.0f,	1.0f,		1.0f, 0.0f // top   
 };
 
 GLuint indices[] = {
@@ -57,14 +57,41 @@ int main() {
 	VBO VBO1(vertices, sizeof(vertices));
 	EBO EBO1(indices, sizeof(indices));
 
-	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 6 * sizeof(float), (void*)0);
-	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 8 * sizeof(float), (void*)0);
+	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	VAO1.LinkAttrib(VBO1, 2, 2, GL_FLOAT, 8 * sizeof(float), (void*)(6 * sizeof(float)));
 	VAO1.Unbind();
 	VBO1.Unbind();
 	EBO1.Unbind();
 
 	GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
 
+	//Texture
+	int widthImg, heightImg, numColCh;
+	unsigned char* bytes = stbi_load("./assets/textures/pop_cat.png", &widthImg, &heightImg, &numColCh, 0);
+	std::cout << "Width: " << widthImg << " Height: " << heightImg << " Number of color channels: " << numColCh << std::endl;
+
+	GLuint texture;
+	glGenTextures(1, &texture);
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	// float flatColor[] = { 1.0f, 0.0f, 0.0f, 1.0f };
+	// glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, flatColor);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, widthImg, heightImg, 0, GL_RGBA, GL_UNSIGNED_BYTE, bytes);
+	glGenerateMipmap(GL_TEXTURE_2D);
+
+	stbi_image_free(bytes);
+	glBindTexture(GL_TEXTURE_2D, 0);
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+	GLuint tex0Uni = glGetUniformLocation(shaderProgram.ID, "tex0");
+	shaderProgram.Activate();
+	glUniform1i(tex0Uni, 0);
 	// Main loop
 	while (!glfwWindowShouldClose(window)) {
 		// Render here (clear the screen)
@@ -73,6 +100,7 @@ int main() {
 		// Draw the triangle
 		shaderProgram.Activate();
 		glUniform1f(uniID, 0.5f);
+		glBindTexture(GL_TEXTURE_2D, texture);
 		VAO1.Bind();
 		//	glDrawArrays(GL_TRIANGLES, 0, 3);
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
@@ -86,6 +114,7 @@ int main() {
 	VAO1.Delete();
 	VBO1.Delete();
 	EBO1.Delete();
+	glDeleteTextures(1, &texture);
 	shaderProgram.Delete();
 	// Clean up and exit
 	glfwDestroyWindow(window);
