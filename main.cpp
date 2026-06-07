8f698e1048857fee03fb664acece469d8ebd8b46
ltaparkrunner <ltaparkrunner@gmail.com>
2026-06-07 14:12:35 +0300

    An orange triangle.

--- main.cpp
+++ main.cpp
@@ -2,12 +2,14 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+// Vertex shader source code
 const char* vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
 "void main()\n"
 "{\n"
 "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
 "}\0";
+// Fragment shader source code
 const char* fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
 "void main()\n"
@@ -25,6 +27,13 @@ int main() {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	// Define the vertices of a triangle
+	GLfloat vertices[] = {
+		-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // left  
+		 0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // right 
+		 0.0f,  0.5f * float(sqrt(3)) * 2 / 3, 0.0f  // top   
+	};
+
 	// Create a windowed mode window and its OpenGL context
 	GLFWwindow* window = glfwCreateWindow(800, 800, "YoutubeOpenGL Window", nullptr, nullptr);
 	if (!window) {
@@ -43,7 +52,41 @@ int main() {
 	gladLoadGL();
 	// Set the viewport and clear the screen with a color
 	glViewport(0, 0, 800, 800);
-	//glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+	
+	// Build and compile our shader program
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
+	glCompileShader(vertexShader);
+	// Create and compile the fragment shader
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
+	glCompileShader(fragmentShader);
+	// Link shaders to create a shader program
+	GLuint shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	// Delete the shader objects once we've linked them into the program
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	// Set up vertex data and buffers and configure vertex attributes
+	GLuint VBO, VAO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	// Bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	// Configure vertex attributes so that OpenGL knows how to interpret the vertex data
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	// Note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+
+
 	glClearColor(0.27f, 0.33f, 0.37f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
 	glfwSwapBuffers(window);
@@ -51,12 +94,26 @@ int main() {
 	// Main loop
 	while (!glfwWindowShouldClose(window)) {
 		// Render here (clear the screen)
+		glClearColor(0.27f, 0.33f, 0.37f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+		// Draw the triangle
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
 		// Swap front and back buffers
 		glfwSwapBuffers(window);		
 		// Poll for and process events
 		glfwPollEvents();
 	}
+
+	// Deallocate resources
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	//glDeleteBuffers(1, &shaderProgram);
+	//glDeleteBuffers(1, &vertexShader);
+	//glDeleteBuffers(1, &fragmentShader);
+	glDeleteProgram(shaderProgram);
 	// Clean up and exit
 	glfwDestroyWindow(window);
 	glfwTerminate();
